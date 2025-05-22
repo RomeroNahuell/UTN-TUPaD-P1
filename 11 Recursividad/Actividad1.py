0b4be27a97a2747771c6a756e3995a4511a07594
romero nahuel <nahuelromero43@outlook.es>
2025-05-22 17:39:24 -0300

    Ejercicios Recursividad

--- 11 Recursividad/Actividad1.py
+++ 11 Recursividad/Actividad1.py
@@ -6,8 +6,7 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * factorial(n-1)
-    
+        return n * factorial(n-1) 
 
 num = int(input("Ingrese un numero: "))
 
--- 11 Recursividad/Actividad5.py
+++ 11 Recursividad/Actividad5.py
@@ -1,19 +1,10 @@
 #Implementá una función recursiva llamada es_palindromo(palabra) que reciba una cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no lo es.
-
 def es_palindromo(palabra):
-    def es_palindromo_rec(palabra, inicio, fin):
-        if inicio >= fin:
-            return True
-        if palabra[inicio] != palabra[fin]:
-            return False
-        return es_palindromo_rec(palabra, inicio + 1, fin - 1)
-    
-    palabra = palabra.lower()
-    return es_palindromo_rec(palabra, 0, len(palabra) - 1)
+    if len(palabra) <= 1:
+        return True
+    if palabra[0] != palabra[-1]:
+        return False
+    return es_palindromo(palabra[1:-1])
 
-
-texto = input("Ingresa una palabra sin espacios ni tildes: ")
-if es_palindromo(texto):
-    print("Es palíndromo")
-else:
-    print("No es palíndromo")
+texto = input("Ingrese un texto: ")
+print(es_palindromo(texto))
